6bcea5aa7985125fd3cc12fd633e1b0d29f77e0c
Gabriel Hofman <gabriel.hofman.00@gmail.com>
2021-11-28 16:22:15 +0100

    fix: better semantic release config

--- release.config.js
+++ release.config.js
@@ -5,5 +5,7 @@ module.exports = {
     '@semantic-release/release-notes-generator',
     '@semantic-release/npm',
     '@semantic-release/git',
+    '@semantic-release/changelog',
+    '@semantic-release/github',
   ],
 };
